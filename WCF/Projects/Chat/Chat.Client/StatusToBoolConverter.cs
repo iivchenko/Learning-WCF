8cf0daa1d7f099f0b97bd7ac04d47fba920f9d6e
shogun <iivchenko@live.com>
2016-01-24 10:35:24 +0000

    Simple Chat improvements
    Fix UI markup
    Fix UI layouts
    Provide UI settings elements bock after connection to a server

---
+++ WCF/Projects/Chat/Chat.Client/StatusToBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Client
+{
+    public class StatusToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            return (ConnectionStatus)value != ConnectionStatus.Connected;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
